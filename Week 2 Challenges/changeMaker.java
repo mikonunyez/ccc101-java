6369a9ed9e160fd309e48a8ba13c0c6406513ede
Miko Nunez <nunyezmiko@gmail.com>
2020-09-28 22:22:58 +0800

    refactored code for changeMaker.java

--- Week 2 Challenges/changeMaker.java
+++ Week 2 Challenges/changeMaker.java
@@ -1,32 +1,23 @@
+/*
+Refactored the code on Sept 28 to use global variables
+instead of declaring new variables that serve the same purpose
+*/
+
 import java.util.Scanner;
 
 class changeMaker{
-    public static void userInput(){ //called for user inputs
-        int input;
+    static int amount;
 
+    //called for user inputs
+    public static void userInput(){ 
         Scanner keyInput = new Scanner(System.in);
 
-        System.out.printf("Enter a whole number from 1 to 10000 (cents)\nI will find a combination of coins that equals that amount of change: ");
-        input = keyInput.nextInt();
-
-
-
-        /*
-        The followign if/else statement serves as a checker
-        Ensuring that the user follows instructions
-        Users can't be trusted
-        */
-
-        if (input>10000 || input<1){
-            System.out.println("You have entered an invalid amount. Try again. \n\n");
-            userInput();
-        }
-        else {
-            changeCalc(input);
-        }
+        System.out.printf("Enter a whole number from 1 to 10000 (cents)\nI will find a combination of denominations that equals that amount of change: ");
+        amount = keyInput.nextInt();
     }
 
-    public static void changeCalc(int amount){ //calculates the change
+    //calculates the change
+    public static void changeCalc(){ 
         int thousands, fiveHundreds, twoHundreds, oneHundreds, quarters, dimes, nickels, pennies, temp;
 
         temp = amount;
@@ -65,18 +56,18 @@ class changeMaker{
         System.out.println(pennies + " pennies");
     }
 
-    public static void main(String[] args){
-        /*
-        I have limited the code that the main method runs
-        This is a habit I picked up a while ago
-        This should help in the security of data within the code...but for the purposes of this specific code,
-            all methods are public... still a good habit in my opinion
 
-        This also reduces the "spaghetti code" effect when everything is under the main method
-        Althought it would be better to separate them into classes
-        */
-        userInput();
+    //main method here
+    public static void main(String[] args){
+        changeMaker coins = new changeMaker();
+        coins.userInput();
+
+        if (amount>10000 || amount<1){
+            System.out.println("You have entered an invalid amount. Try again. \n\n");
+            coins.userInput();
+        }
+        else {
+            coins.changeCalc();
+        }
     }
 }
-
-//signed, Miko
